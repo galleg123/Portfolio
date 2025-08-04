1cea05b5ae07268082888c1ad36ab353ce2a3bd1
Mikkel Silvasen <mikkelkp360@gmail.com>
2025-08-04 11:03:42 +0200

    Sidebar commit, now with content

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,20 @@
+import styles from "./Sidebar.module.css";
+
 const Sidebar = () => {
   return (
     <>
-      <div className="sidebar">
-        <h2>Sidebar</h2>
+      <div className={styles.sidebar}>
+        <ul className={styles.sidebarList}>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
       </div>
     </>
   );
